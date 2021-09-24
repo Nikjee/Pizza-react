af4f1c8d3de17cd29fd323043c80cc3e8534ae61
Nikjee <boss.bold@mail.ru>
2021-09-24 19:42:43 +0300

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { Route, Router } from 'react-router-dom'
+import React from 'react'
+import axios from 'axios'
+
+import {Header} from './components'
+import {Home, Cart} from './pages'
 
 function App() {
+  const [pizzas, setPizzas] = React.useState([])
+
+  React.useEffect(() => {
+    axios.get('http://localhost:3001/db.json').then(({data}) => {
+      setPizzas(data.pizzas)
+    })
+    
+  })
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='wrapper'>
+      <Header />
+      <div className='content'>
+        <Route path='/' render={() => <Home items={pizzas} />} exact/>
+        <Route path='/cart' component={Cart} exact/>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
