76dc5b5443f18bf74873c64ac77a189ae4f59ffe
Nikjee <boss.bold@mail.ru>
2021-10-08 03:45:04 +0300

    added proptypes

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,29 +1,36 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
-function Categories({items}) {
+const Categories = React.memo(function Categories({ activeCategory,items,onClickCategory }) {
 
-  const [activeItem,setActiveItem] = React.useState(null)
-
-  const onSelectItem = (index) => {
-    setActiveItem(index)
-  }
     return (
         <div className='categories'>
               <ul>
                 <li 
-                className={activeItem === null ? 'active' : ''}
-                onClick={() => setActiveItem(null)}>Все</li>
+                className={activeCategory === null ? 'active' : ''}
+                onClick={() => onClickCategory(null)}>Все</li>
                 {items &&
-                    items.map((name,index) => <li
-                    className={activeItem === index ? 'active' : ''}
-                    onClick={() => onSelectItem(index)}
+                    items.map((name,index) => (<li
+                    className={activeCategory === index ? 'active' : ''}
+                    onClick={() => onClickCategory(index)}
                     key={`${name}_${index}`}>
                       {name}
-                    </li>)
+                    </li>))
                 }
               </ul>
             </div>
     )
+})
+
+Categories.propTypes = {
+  // activeCategory: PropTypes.oneOf([PropTypes.number,null]),
+  items: PropTypes.arrayOf(PropTypes.string).isRequired,
+  onClickCategory: PropTypes.func
+}
+
+Categories.defaultProps = {
+  activeCategory:null,
+  items: [],
 }
 
 export default Categories
