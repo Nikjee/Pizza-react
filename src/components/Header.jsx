eaf7d3d8298d67a26a3c96772f17d1cdca7ee17a
Nikjee <boss.bold@mail.ru>
2021-10-08 03:46:19 +0300

    added redux

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
+import { useSelector } from 'react-redux'
 
 import logoSvg from '../assets/img/pizza-logo.svg'
 import Button from './Button'
 
-function Header(){
-    return(
-        <div className='header'>
-        <div className='container'>
-          <Link to='/'>
+function Header() {
+  const { totalPrice, totalCount } = useSelector(({ cart }) => cart)
+  return (
+    <div className='header'>
+      <div className='container'>
+        <Link to='/'>
           <div className='header__logo'>
             <img width='38' src={logoSvg} alt='Pizza logo' />
             <div>
@@ -16,11 +18,11 @@ function Header(){
               <p>самая вкусная пицца во вселенной</p>
             </div>
           </div>
-          </Link>
-          <div className='header__cart'>
-            <Link to="/cart">
-            <Button className="button--cart">
-            <span>520 ₽</span>
+        </Link>
+        <div className='header__cart'>
+          <Link to='/cart'>
+            <Button className='button--cart'>
+              <span>{totalPrice} р</span>
               <div className='button__delimiter'></div>
               <svg
                 width='18'
@@ -51,13 +53,13 @@ function Header(){
                   strokeLinejoin='round'
                 />
               </svg>
-              <span>3</span>
+              <span>{totalCount}</span>
             </Button>
-            </Link>
-          </div>
+          </Link>
         </div>
       </div>
-    )
+    </div>
+  )
 }
 
 export default Header
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,35 +1,84 @@
 import React from 'react'
+import { useSelector, useDispatch } from 'react-redux'
 
-import {Categories, SortPopup, PizzaBlock} from '../components'
+import {
+  Categories,
+  SortPopup,
+  PizzaBlock,
+  PizzaLoadingBlock,
+} from '../components'
 
-function Home({items}) {
-    return (
-        <div className='container'>
-          <div className='content__top'>
-          <Categories items={[
-              'Мясные',
-              'Вегетарианская',
-              'Гриль',
-              'Острые',
-              'Закрытые'
-            ]} />
-            <SortPopup items={[
-              'популярности',
-              'цене',
-              'алфавиту'
-            ]}/>
-          </div>
-          <h2 className='content__title'>Все пиццы</h2>
-          <div className='content__items'>
-            {
-              items.map(obj =>( 
-                  <PizzaBlock key={obj.id} {...obj} />
-                )
-              )
-            }
-           </div>
-        </div>
-    )
+import { setCategory, setSortBy } from '../redux/actions/filters'
+import { fetchPizzas } from '../redux/actions/pizzas'
+import { addPizzaToCart } from '../redux/actions/cart'
+
+const categotyNames = [
+  'Мясные',
+  'Вегетарианская',
+  'Гриль',
+  'Острые',
+  'Закрытые',
+]
+const sortItems = [
+  { name: 'популярности', type: 'popular', order: 'desc' },
+  { name: 'цене', type: 'price', order: 'desc' },
+  { name: 'алфавиту', type: 'name', order: 'asc' },
+]
+
+function Home() {
+  const dispatch = useDispatch()
+  const items = useSelector(({ pizzas }) => pizzas.items)
+  const cartItems = useSelector(({ cart }) => cart.items)
+  const isLoaded = useSelector(({ pizzas }) => pizzas.isLoaded)
+  const { category, sortBy } = useSelector(({ filters }) => filters)
+
+  React.useEffect(() => {
+    dispatch(fetchPizzas(sortBy, category))
+  }, [category, sortBy])
+
+  const onSelectCategory = React.useCallback((index) => {
+    dispatch(setCategory(index))
+  }, [])
+
+  const onSelectSortType = React.useCallback((type) => {
+    dispatch(setSortBy(type))
+  }, [])
+
+  const handleAddPizzaToCart = (obj) => {
+    dispatch(addPizzaToCart(obj))
+  }
+
+  return (
+    <div className='container'>
+      <div className='content__top'>
+        <Categories
+          activeCategory={category}
+          onClickCategory={onSelectCategory}
+          items={categotyNames}
+        />
+        <SortPopup
+          onClickSortType={onSelectSortType}
+          activeSortType={sortBy.type}
+          items={sortItems}
+        />
+      </div>
+      <h2 className='content__title'>Все пиццы</h2>
+      <div className='content__items'>
+        {isLoaded
+          ? items.map((obj) => (
+              <PizzaBlock
+                onClickAddPizza={handleAddPizzaToCart}
+                key={obj.id}
+                addedCount={cartItems[obj.id] && cartItems[obj.id].items.length}
+                {...obj}
+              />
+            ))
+          : Array(12)
+              .fill(0)
+              .map((_, index) => <PizzaLoadingBlock key={index} />)}
+      </div>
+    </div>
+  )
 }
 
 export default Home
