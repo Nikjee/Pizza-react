30d58a40b53dc3ce1b858e707dfbfd54a273d12b
Nikjee <boss.bold@mail.ru>
2021-10-08 03:43:16 +0300

    added redux

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
 import React from 'react'
-import { Route, Router } from 'react-router-dom'
-import React from 'react'
-import axios from 'axios'
+import { Route } from 'react-router-dom'
 
 import {Header} from './components'
 import {Home, Cart} from './pages'
 
-function App() {
-  const [pizzas, setPizzas] = React.useState([])
 
-  React.useEffect(() => {
-    axios.get('http://localhost:3001/db.json').then(({data}) => {
-      setPizzas(data.pizzas)
-    })
-    
-  })
+function App() {
+ 
   return (
     <div className='wrapper'>
       <Header />
       <div className='content'>
-        <Route path='/' render={() => <Home items={pizzas} />} exact/>
+        <Route path='/' component={Home} exact/>
         <Route path='/cart' component={Cart} exact/>
       </div>
     </div>
